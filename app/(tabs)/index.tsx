81febda63178804dd6b15cf0d27d3b7bda49fbfd
josh098741 <morongemokaya@gmail.com>
2025-12-21 19:08:28 +0300

    Additional commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,17 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+   const { toggleDarkMode } = useTheme()
    return (
       <View
          style={styles.container}
       >
          <Text style={styles.content} >Edit app/index.tsx to edit this screen.</Text>
          <Text>Hi</Text>
+         <TouchableOpacity onPress={toggleDarkMode} >
+            <Text>Toggle The Mode</Text>
+         </TouchableOpacity>
       </View>
    );
 }
