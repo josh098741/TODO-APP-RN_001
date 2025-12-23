8bbc58ea171dd843d22b7f99b86c3f7a5f92d1fd
josh098741 <morongemokaya@gmail.com>
2025-12-23 11:10:02 +0300

    Additional commits

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
    const { toggleDarkMode } = useTheme()
+
    return (
       <View
          style={styles.container}
