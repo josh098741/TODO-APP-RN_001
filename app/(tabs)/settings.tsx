1064cab380391896d03f5370b7867aeb14cc33fc
josh098741 <morongemokaya@gmail.com>
2025-12-23 23:27:37 +0300

    Additional commit

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,4 +1,5 @@
 import { createSettingsStyles } from "@/assets/styles/settings.styles"
+import DangerZone from "@/components/DangerZone"
 import Preferences from "@/components/Preferences"
 import ProgressStats from "@/components/ProgressStats"
 import useTheme from "@/hooks/useTheme"
@@ -35,6 +36,8 @@ const SettingsScreen = () => {
 
                      {/* PREFERENCES */}
                      <Preferences />
+
+                     <DangerZone />
                 </ScrollView>
             </SafeAreaView>
         </LinearGradient>
