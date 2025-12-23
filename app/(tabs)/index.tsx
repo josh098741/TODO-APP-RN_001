8b5c22523621ae36263c45aac48442097d8352c1
josh098741 <morongemokaya@gmail.com>
2025-12-23 12:51:23 +0300

    Additional coommit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,26 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
 import useTheme from "@/hooks/useTheme";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient"
+import Header from "@/components/Header";
 
 export default function Index() {
-   const { toggleDarkMode } = useTheme()
+   const { toggleDarkMode,colors } = useTheme()
 
+   const homeStyles = createHomeStyles(colors)
    return (
-      <View
-         style={styles.container}
-      >
-         <Text style={styles.content} >Edit app/index.tsx to edit this screen.</Text>
-         <Text>Hi</Text>
-         <TouchableOpacity onPress={toggleDarkMode} >
-            <Text>Toggle The Mode</Text>
-         </TouchableOpacity>
-      </View>
+      <LinearGradient colors={colors.gradients.background} style={homeStyles.container} >
+         <StatusBar barStyle={colors.statusBarStyle} />
+         <SafeAreaView
+            style={homeStyles.safeArea}
+         >
+            <Header />
+            <TouchableOpacity onPress={toggleDarkMode} >
+               <Text>Toggle The Mode</Text>
+            </TouchableOpacity>
+         </SafeAreaView>
+      </LinearGradient>
+      
    );
 }
-
-const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      gap: 10
-   },
-   content: {
-      fontSize: 22
-   }
-})
---
+++ components/Header.tsx
@@ -0,0 +1,52 @@
+import { createHomeStyles } from "@/assets/styles/home.styles"
+import { api } from "@/convex/_generated/api"
+import { useQuery } from "convex/react"
+import { View, Text } from "react-native"
+import useTheme from "@/hooks/useTheme"
+import { LinearGradient } from "expo-linear-gradient"
+import { Ionicons } from "@expo/vector-icons"
+
+
+const Header = () => {
+    const { colors } = useTheme()
+
+    const homeStyles = createHomeStyles(colors)
+    const todos = useQuery(api.todos.getTodos)
+
+    const completedCount = todos ? todos.filter((todo) => todo.isCompleted).length: 0
+    const totalCount = todos ? todos.length : 0;
+    const progressPercantage = totalCount > 0 ? (completedCount / totalCount) * 100 : 0
+
+    return(
+        <View style={homeStyles.header} >
+            <View style={homeStyles.titleContainer} >
+                <LinearGradient colors={colors.gradients.primary} style={homeStyles.iconContainer}>
+                    <Ionicons name="flash-outline" size={28} color="#fff" />
+                </LinearGradient>
+                <View style={homeStyles.titleTextContainer} >
+                    <Text style={homeStyles.title} >Today&apos;s Tasks 😃</Text>
+                    <Text style={homeStyles.subtitle} >
+                        {completedCount} 0f {totalCount} completed
+                    </Text>
+                </View>
+            </View>
+            {
+                totalCount > 0 && (
+                    <View style={homeStyles.progressContainer} >
+                        <View style={homeStyles.progressBarContainer} >
+                            <View style={homeStyles.progressBar} >
+                                <LinearGradient 
+                                    colors={colors.gradients.success}
+                                    style={[homeStyles.progressFill, {width: `${progressPercantage}%`}]}
+                                />
+                            </View>
+                            <Text style={homeStyles.progressText} >{Math.round(progressPercantage)}%</Text>
+                        </View>
+                    </View>
+                )
+            }
+        </View>
+    )
+}
+
+export default Header
